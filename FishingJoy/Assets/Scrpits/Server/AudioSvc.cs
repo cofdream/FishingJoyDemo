e9e42aa1b32ab18f72dceaab3aa6d25e11558c89
cq <cofdream@sina.com>
2019-04-02 23:12:08 +0800

    .

--- FishingJoy/Assets/Scrpits/Server/AudioSvc.cs
+++ FishingJoy/Assets/Scrpits/Server/AudioSvc.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//
-public class AudioSvc : MonoBehaviour {
+//音效管理
+public class AudioSvc : MonoBehaviour
+{
+
+    public AudioSvc Instance { get; private set; }
+
+    private AudioSource BgAS;
+    private AudioSource UIAS;
+    private AudioSource AS;
+
+    public void InitSvc()
+    {
+        Instance = this;
+        BgAS = transform.Find("BgAudioSource").GetComponent<AudioSource>();
+        UIAS = transform.Find("UIAudioSource").GetComponent<AudioSource>();
+        AS = transform.Find("SceneAudioSource").GetComponent<AudioSource>();
+    }
+    //背景音乐
+    public void PlayerBgAudio(string pathClip, bool loop = true)
+    {
+
+    }
+    //UI按钮音效/打开面板音效
+    public void PlayerUIAudio(string pathClip, bool loop = false)
+    {
+
+    }
 
 
-    private void Start () {
-		
-	}
-	
-	private void Update () {
-		
-	}
+
 }
